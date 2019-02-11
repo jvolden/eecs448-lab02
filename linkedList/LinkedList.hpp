9e5693a827c6258c46eeac53e0ae2b99968878d8
Jon Volden <volden.jon@gmail.com>
2019-02-11 00:58:39 +0000

    Updated search() function

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -38,9 +38,19 @@ bool LinkedList<T>::search(T value) const
 	Node<T>* temp = m_front;
 	bool isFound = false;
 
-	/** TODO 
-		Fix this method
-	*/
+	while (temp != nullptr)
+	{
+		if (temp->getValue() == value)
+		{
+			isFound = true;
+			break;
+		}
+		else
+		{
+			temp = temp->getNext();
+		}
+		
+	}
 
 	return(isFound);
 }
